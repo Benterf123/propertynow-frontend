452979d23d464b117d67003b28450377275de62c
Benter <faithbenter@gmail.com>
2023-06-16 16:37:42 +0300

    API cleanup

--- src/core/api/interceptors.ts
+++ src/core/api/interceptors.ts
@@ -1,135 +1,63 @@
-import axios, { AxiosError, type AxiosResponse } from "axios";
-import type { AxiosRequestConfig } from "axios";
+import axios, { AxiosError, type AxiosResponse } from 'axios'
+import type { AxiosRequestConfig } from 'axios'
 
-import { TokenCategory } from "@/common/constants";
-import {
-  isAuthTokenValid,
-  retrieveAuthToken,
-  setNewAccessToken,
-} from "@/common/functional";
-import { baseURL } from "./api";
+import { TokenCategory } from '@/common/constants'
+import { retrieveAuthToken } from '@/common/functional'
 
-type TRequestInterceptorResponse =
-  | AxiosRequestConfig
-  | Promise<AxiosRequestConfig>;
+type TRequestInterceptorResponse = AxiosRequestConfig | Promise<AxiosRequestConfig>
 
-let isRefreshingToken = false;
-export function authInterceptor(
-  config: AxiosRequestConfig
-): TRequestInterceptorResponse {
-  const accessToken = retrieveAuthToken(TokenCategory.Access);
-
-  if (accessToken) {
-    // eslint-disable-next-line no-constant-condition
-    if (false) {
-      // if (!isAuthTokenValid(5 * 60 * 1000)) {
-      console.log("Invalid Token", config.url);
-
-      return new Promise((resolve, reject) => {
-        reject(Error("Token refresh not handled"));
-        console.log("Refreshing...", config.url);
-
-        const refreshToken = retrieveAuthToken(TokenCategory.Refresh);
-        if (!isRefreshingToken) {
-          isRefreshingToken = true;
-
-          axios
-            .post("/users/token", { token: refreshToken }, { baseURL })
-            .then((response) => {
-              console.log("Token Refreshed for", config.url);
-
-              const newAccessToken = response.data.data.accessToken as string;
-              setNewAccessToken(newAccessToken);
-
-              if (config.headers) {
-                config.headers.Authorization = `Bearer ${newAccessToken}`;
-              }
-
-              isRefreshingToken = false;
-              resolve(config);
-            })
-            .catch((err) => {
-              console.error("Error while refreshing\n", err);
-
-              reject(err);
-            });
-        } else {
-          const interval = setInterval(() => {
-            if (isAuthTokenValid()) {
-              clearInterval(interval);
-              console.log("Token is valid for request", config.url);
-
-              const accessToken = retrieveAuthToken(TokenCategory.Access);
-              if (config.headers) {
-                config.headers.Authorization = `Bearer ${accessToken}`;
-              }
-              resolve(config);
-            }
-          }, 500);
-        }
-      });
-    } else if (config.headers) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
+export function authInterceptor(config: AxiosRequestConfig): TRequestInterceptorResponse {
+  const accessToken = retrieveAuthToken(TokenCategory.Access)
+  if (accessToken && config.headers) {
+    config.headers.Authorization = `Bearer ${accessToken}`
   }
 
-  return config;
+  return config
 }
 
 function logObject(title: string, obj?: Record<string, any>): void {
-  if (obj === undefined) return console.log(`${title}: undefined`);
+  if (obj === undefined) return console.log(`${title}: undefined`)
 
-  console.groupCollapsed(title);
+  console.groupCollapsed(title)
   for (const [key, param] of Object.entries(obj)) {
-    console.log(`${key}: `, param);
+    console.log(`${key}: `, param)
   }
 
-  console.groupEnd();
+  console.groupEnd()
 }
 
-const isDevMode = import.meta.env.MODE === "development";
-export function requestLoggerInterceptor(
-  config: AxiosRequestConfig
-): TRequestInterceptorResponse {
+const isDevMode = import.meta.env.MODE === 'development'
+export function requestLoggerInterceptor(config: AxiosRequestConfig): TRequestInterceptorResponse {
   if (isDevMode) {
-    console.groupCollapsed(
-      `Request  (${config.method?.toUpperCase()}):`,
-      config.url
-    );
+    console.groupCollapsed(`Request  (${config.method?.toUpperCase()}):`, config.url)
 
-    console.log("Uri: ", axios.getUri(config));
-    logObject("Params", config.params);
-    logObject("Data", config.data);
+    console.log('Uri: ', axios.getUri(config))
+    logObject('Params', config.params)
+    logObject('Data', config.data)
 
-    console.groupEnd();
+    console.groupEnd()
   }
 
-  return config;
+  return config
 }
 
 export const responseLoggerInterceptor = {
   success(response: AxiosResponse): AxiosResponse {
-    if (!isDevMode) return response;
+    if (!isDevMode) return response
 
-    console.groupCollapsed(
-      `Response (${response.status}):`,
-      response.config.url
-    );
-    console.log("Data: ", response.data);
-    console.groupEnd();
+    console.groupCollapsed(`Response (${response.status}):`, response.config.url)
+    console.log('Data: ', response.data)
+    console.groupEnd()
 
-    return response;
+    return response
   },
   error(err: AxiosError): any {
-    if (!isDevMode) return Promise.reject(err);
+    if (!isDevMode) return Promise.reject(err)
 
-    console.groupCollapsed(
-      `Response (${err.response?.status}):`,
-      err.config?.url
-    );
-    console.error("Data:", err.response?.data);
-    console.groupEnd();
+    console.groupCollapsed(`Response (${err.response?.status}):`, err.config?.url)
+    console.error('Data:', err.response?.data)
+    console.groupEnd()
 
-    return Promise.reject(err);
-  },
-};
+    return Promise.reject(err)
+  }
+}
